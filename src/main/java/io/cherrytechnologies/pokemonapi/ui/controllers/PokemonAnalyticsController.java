a58133097f3178a867a4f945a5032a11d343f10e
RajanBaliyan <rajansinghbaliyan@gmail.com>
2022-09-10 05:05:34 +0530

    analytics controller

---
+++ src/main/java/io/cherrytechnologies/pokemonapi/ui/controllers/PokemonAnalyticsController.java
@@ -0,0 +1,10 @@
+package io.cherrytechnologies.pokemonapi.ui.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/v1/api/pokemons/analytics")
+public class PokemonAnalyticsController {
+
+}
